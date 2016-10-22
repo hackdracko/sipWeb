05ffba04c8834301a80df73b0f7c9517159dc465
HAck Dracko <HAck Dracko>
2016-10-22 16:39:12 -0500

    Se creo la tabla de usuarios

---
+++ database/migrations/2016_10_22_211126_create_usuarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('idAsesor');
+            $table->string('usuario');
+            $table->string('password');
+            $table->integer('administrador');
+            $table->integer('activo');
+            $table->dateTime('fechaExpiracion');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('usuarios');
+    }
+}
